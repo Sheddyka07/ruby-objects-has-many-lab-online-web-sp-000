f1a1334aa87fe80da312bf2fb31b34a13819a506
Kelly Sheddy <jungaka07@gmail.com>
2018-12-31 14:19:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,13 @@
 class Post
-
-  attr_accessor :title, :author
-  def initialize(title)
-    @title = title
-  end
-
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
-  end
-end
+   attr_accessor :author, :title
+ 
+    def initialize(title)
+     @title = title
+   end
+ 
+    def author_name
+     self.author ? self.author.name : nil
+   end
+ end
+ 
+ 
