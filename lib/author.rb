b9c82b59faeb13cecf215e5b92701177cdb797f6
Kelly Sheddy <jungaka07@gmail.com>
2018-12-31 14:20:29 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,34 @@
 class Author
-   attr_accessor :name
- 
-    @@post_count = 0
- 
-    def initialize(name)
-     @name = name
-     @posts = []
-   end
- 
-    def posts
-     @posts
-   end
- 
-    def self.post_count
-     @@post_count
-   end
- 
-    def add_post(post_title)
-     @posts << post_title
-     post_title.author = self
-     @@post_count += 1
-   end
- 
-    def add_post_by_title(post_title)
-     post = Post.new(post_title)
-     @posts << post
-     post.author = self
-     @@post_count += 1
-   end
- end
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def posts
+    @posts
+  end
+
+  def self.all
+    @@all
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+end
